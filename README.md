274ae802bf749cfc2e186d8980def955bf091c8b
Rolando Garcia <jrdosmil0@gmail.com>
2021-11-14 20:27:32 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TSNode
+
+Project to check how works the TS with node js and each librarynpm i -D typescript
+
+tsc --build
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+/// IFFE https://stackoverflow.com/questions/8228281/what-is-the-function-construct-in-javascript
+(function () {
+    console.log("IIFE");
+})()
